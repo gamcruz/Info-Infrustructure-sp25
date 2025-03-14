a9ae21ec0375e0df5b30d0422bbf277c61f1a3f3
Gambo Cruz <gamcruz@iu.edu>
2025-03-14 17:16:49 -0400

    Assignment 4

--- homework-4/journey.js
+++ homework-4/journey.js
@@ -81,7 +81,7 @@ function chooseHero(e) {
     },
 
     declinePath: {
-        text: "Wow, you seriously declined, jerk move but we can move on from it. You continue your shopping and you have the options of buying milk, eggs, sausage, and juice",
+        text: "Wow, you seriously declined, jerk move but we can move on from it. You continue your shopping and you have the options of buying milk, eggs, sausage, and juice. (There's a lot of weight to these options)",
         choices: [
             { text: "Get the milk", next: "milkPath" },
             { text: "Get the eggs", next: "eggPath" },
@@ -90,13 +90,107 @@ function chooseHero(e) {
         ]
         },
     
-    sausagePath: {
-            text: "Awesome the eggs were only $10, which is a lot but at least you have them. Do you wish to continue shopping?",
+    juicePath: {
+            text: "Awesome the juice was only $2, which is a lot but at least you have them. Do you wish to continue shopping?",
             choices: [
-                { text: "Keep Shopping", next: "shoppingPath" },
+                { text: "Keep Shopping", next: "juicierPath" },
                 { text: "No go home", next: "leavePath" }
             ]
         },
+    
+    juicierPath: {
+            text: "Oh, I forgot to mention that this decision leads to an apocalypse. Yes, choosing to continue to shop was a bad idea. That's just great. Well I need to get going, you should too and maybe next time don't end the world?",
+        },
+    
+    
+    sausagePath: {
+            text: "Awesome the sausage were only $7, which is a lot but at least you have them. Do you wish to continue shopping?",
+            choices: [
+                { text: "Keep Shopping", next: "shoppingsausagePath" },
+                { text: "No go home", next: "leavePath" }
+            ]
+        },
+    
+    leavePath: {
+            text: "Then I guess we'll call it a day " + heroName + ", until next time.",  
+        },
+    
+    shoppingsausagePath: {
+            text: "Oh no, your bullies are walking towards you " + heroName + " what are you gonna do?",
+            choices: [
+                { text: "Book it", next: "runfrombulliesPath" },
+                { text: "Show them you're not messing around", next: "fightPath" }
+            ]
+        },
+    
+    runfrombulliesPath: {
+            text: "You successfully got away " + heroName + ", unfortunetly your shopping trip is over now. Until next time",
+            
+        },
+    
+    fightPath: {
+            text: "Okay then " + heroName + ", there's five of them and they're about to jump you. Since you've been watching Rocky recently, these chumps should be nothing, are you still ready to fight?",
+            choices: [
+                { text: "Beg for mercy", next: "nomercyPath" },
+                { text: "Beat them up", next: "beatupPath" }
+            ]
+        },
+    
+    beatupPath: {
+            text: "Okay I need to tell you that these are twelve year olds " + heroName + ", do you still want to fight them?",
+            choices: [
+                { text: "No I'm okay", next: "nofightingPath" },
+                { text: "Beat them up", next: "beatupkidsPath" }
+            ]
+        },
+    
+    beatupkidsPath: {
+            text: "You are a pyscho  " + heroName + ", you beat up kids and called it a day. Such a shame, thought you were different.",
+
+        },
+    
+    nofightingPath: {
+            text: "You fell for it, they are your bullies for a reason " + heroName + ", so the kids beat you up and stole your money. So go home and think about your actions. Until next time.",
+    
+        },
+    
+    nomercyPath: {
+            text: "I hate to break it to you " + heroName + ", since you were originally down to fight them, you don't exactly have a choice. Choose your first move.",
+            choices: [
+                { text: "Run away", next: "failedrunPath" },
+                { text: "Punch the first one who makes a move", next: "punchoutPath" },
+                { text: "Flying Kick", next: "flyingkickPath" }
+            ]
+        },
+    
+    failedrunPath: {
+            text: "Since you let them get too close to you " + heroName + ", your twelve year old bullies beat you up and stole the money you had and the sausages you were going to buy. Since you have no money left, your experience is over. Until next time.",
+            
+        },
+    
+    punchoutPath: {
+            text: "I love the effort " + heroName + ", but your bullies accounted for you swinging like a wuss and beat you up. They left you broke and took your sausage you wanted to buy. Since you have no money left, your experience is over. Until next time.",
+            
+        },
+    
+    flyingkickPath: {
+            text: "Wow you have guts " + heroName + ", your bullies didn't account for the fact you could do a flying kick. You kicked them all in the face like Bruce Lee and successfully beat up twelve year olds. Good job. Do you want juice after that amazing stunt, you have just enough for it?",
+            choices: [
+                { text: "Yes", next: "yesjuicePath" },
+                { text: "No", next: "nojuicePath" }
+            ]
+        },
+    
+    yesjuicePath: {
+            text: "You bought the sausage and juice. You are left with no money " + heroName + ", so you head home and enjoy your victory of the day.",
+
+        },
+    
+    nojuicePath: {
+            text: "You bought the sausage and you are left with no money " + heroName + ", so you head home and enjoy your victory of the day.",
+
+        },
+    
     
         eggPath: {
             text: "Awesome the eggs were only $10, which is a lot but at least you have them. Do you wish to continue shopping?",
